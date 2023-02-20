1a65bfd8b34983a2252b8b2c8305407f49095133
Jothi Ramaswamy <jothiramaswamy@dhcp-10-250-35-25.harvard.edu>
2023-02-20 15:40:57 -0500

    started gRPC and started server code

---
+++ Messenger/gRPCProtocol/chat_pb2.pyi
@@ -0,0 +1,43 @@
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+ACCOUNT_ALREADY_EXISTS: ServerOperation
+ACCOUNT_DOES_NOT_EXIST: ServerOperation
+CREATE_ACCOUNT: ClientOperation
+DELETE_ACCOUNT: ClientOperation
+DESCRIPTOR: _descriptor.FileDescriptor
+FAILURE: ServerOperation
+LIST_ACCOUNTS: ClientOperation
+LIST_OF_ACCOUNTS: ServerOperation
+LIST_OF_MESSAGES: ServerOperation
+LOGIN: ClientOperation
+LOGOUT: ClientOperation
+QUIT_MESSENGER: ClientOperation
+RECEIVE_CURRENT_MESSAGE: ClientOperation
+SEND_MESSAGE: ClientOperation
+SUCCESS: ServerOperation
+VIEW_UNDELIVERED_MESSAGES: ClientOperation
+
+class ClientMessage(_message.Message):
+    __slots__ = ["info", "operation"]
+    INFO_FIELD_NUMBER: _ClassVar[int]
+    OPERATION_FIELD_NUMBER: _ClassVar[int]
+    info: str
+    operation: ClientOperation
+    def __init__(self, operation: _Optional[_Union[ClientOperation, str]] = ..., info: _Optional[str] = ...) -> None: ...
+
+class ServerMessage(_message.Message):
+    __slots__ = ["info", "operation"]
+    INFO_FIELD_NUMBER: _ClassVar[int]
+    OPERATION_FIELD_NUMBER: _ClassVar[int]
+    info: str
+    operation: ServerOperation
+    def __init__(self, operation: _Optional[_Union[ServerOperation, str]] = ..., info: _Optional[str] = ...) -> None: ...
+
+class ServerOperation(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
+
+class ClientOperation(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
---
+++ Messenger/gRPCProtocol/chatservice_server.py
@@ -0,0 +1,116 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter server."""
+
+from concurrent import futures
+import logging
+import threading
+
+import grpc
+import chat_pb2
+import chat_pb2_grpc
+from user import User
+from gRPCProtocol.chat_pb2 import ClientOperation, ServerOperation
+
+
+class ChatService(chat_pb2_grpc.ChatServiceServicer):
+
+    PORT = 5050
+    SERVER_HOST = '10.250.35.25' # gets host IPv4 address
+    HEADER = 64
+    ADDR = (SERVER_HOST, PORT)
+    FORMAT = "utf-8"
+    DISCONNECT_MESSAGE = "!DISCONNECT"
+    SEPARATE_CHARACTER = "\n"
+    VERSION = "1"
+
+    USER_LOCK = threading.Lock()
+
+    USERS = {} # dictionary holding all user objects { key: username, value: user object}
+    ACTIVE_USERS = {} # holds currently logged in users { key: username, value: conn}
+
+    def LoginClient(self, request, context):
+        with self.USER_LOCK:
+            if request.info in self.USERS:
+                self.ACTIVE_USERS[request.info] = request.info
+                return chat_pb2.ServerMessage(operation=ServerOperation.Value("SUCCESS"), info="")
+        return chat_pb2.ServerMessage(operation=ServerOperation.Value("FAILURE"), info="")
+
+    def CreateAccountClient(self, request, context):
+        with self.USER_LOCK:
+            if request.info in self.USERS:
+                return chat_pb2.ServerMessage(operation=ServerOperation.Value("ACCOUNT_ALREADY_EXISTS"), info="")
+            new_user = User(request.info)
+            self.USERS[request.info] = new_user
+            self.ACTIVE_USERS[request.info] = request.info
+        return chat_pb2.ServerMessage(operation=ClientOperation.Value("SUCCESS"), info="")
+
+    def DeleteAccountClient(self, request, context):
+        with self.USER_LOCK:
+            if request.info in self.USERS and request.info in self.ACTIVE_USERS:
+                del self.USERS[request.info]
+                del self.ACTIVE_USERS[request.info]
+                return chat_pb2.ServerMessage(operation=ClientOperation.Value("SUCCESS"), info="")
+        return chat_pb2.ServerMessage(operation=ServerOperation.Value("ACCOUNT_DOES_NOT_EXIST"), info="")
+
+    def ListAccountClient(self, request, context):
+        with self.USER_LOCK:
+            if not self.USERS:
+                return chat_pb2.ServerMessage(operation=ServerOperation.Value("FAILURE"), info="")
+            else:
+                accounts = self.USERS.keys()
+                accounts_string = "\n".join(accounts)
+                return chat_pb2.ServerMessage(operation=ClientOperation.Value("SUCCESS"), info=accounts_string)
+    
+    def SendMessageClient(self, request, context):
+        sender, receiver, msg = request.info.split("\n")
+        with self.USER_LOCK:
+            if receiver in self.USERS:
+                if receiver not in self.ACTIVE_USERS:
+                    self.USERS[receiver].queue_message(msg)
+                    return chat_pb2.ServerMessage(operation=ClientOperation.Value("SUCCESS"), info="")
+            else:
+                return chat_pb2.ServerMessage(operation=ServerOperation.Value("FAILURE"), info="")
+
+    def ViewMessageClient(self, request, context):
+        with self.USER_LOCK:
+            if not self.USERS[request.info].undelivered_messages: # handle case of no undelivered messages
+                return chat_pb2.ServerMessage(operation=ServerOperation.Value("FAILURE"), info="")
+            messages = self.SEPARATE_CHARACTER.join(self.USERS[request.info].get_current_messages())
+        return chat_pb2.ServerMessage(operation=ClientOperation.Value("SUCCESS"), info=messages)
+
+    def LogoutClient(self, request, context):
+        with self.USER_LOCK:
+            if request.info in self.ACTIVE_USERS:
+                del self.ACTIVE_USERS[request.info]
+                return chat_pb2.ServerMessage(operation=ClientOperation.Value("SUCCESS"), info="")
+        return chat_pb2.ServerMessage(operation=ClientOperation.Value("FAILURE"), info="")
+
+    def QuitClient(self, request, context):
+        return chat_pb2.ServerMessage(operation=ClientOperation.Value("QUIT_MESSENGER"), info="")
+
+
+def serve():
+    port = '50051'
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    chat_pb2_grpc.add_ChatServiceServicer_to_server(ChatService(), server)
+    server.add_insecure_port('[::]:' + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
